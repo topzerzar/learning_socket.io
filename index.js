f02e85a5b00c93d985f14aaaa51a5c21c3f58a29
Weerapon <top.collection.it@gmail.com>
2018-01-14 14:58:19 +0700

    add chat html

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ let app = express();
 
 let PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
